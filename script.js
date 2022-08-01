62c16d1ac25c92638d81dddd1bff34b2a34fd17d
Eyouel <eeyoueltsegaye@gmail.com>
2022-08-01 23:09:34 +0300

    updated the JS file to partially do the logic

--- script.js
+++ script.js
@@ -1,112 +1,166 @@
-
-
-
 var player= 1
 var clickedBoxes = {}
 var turn = false;
+var blank = [[0,0,0],[0,0,0],[0,0,0],
+             [0,0,0],[0,0,0],[0,0,0],
+             [0,0,0],[0,0,0],[0,0,0],] 
+
 function turnGiver(){
     if(turn){
         document.querySelector("body").setAttribute("style", "background-color: red;");
 
         turn = !turn;
         document.querySelector("#player").innerHTML = "<h1> player 1's turn</h1>"
+        if(player==1){
+            player=2
+        }
+        else if(player==2){
+            player=1
+        }
     }
     else{
         document.querySelector("body").setAttribute("style", "background-color: blue;");
 
         turn = !turn;
         document.querySelector("#player").innerHTML = "<h1> player 2's turn</h1>"
+        if(player==1){
+            player=2
+        }
+        else if(player==2){
+            player=1
+        }
     }
 
 }
+function marked(row, column , player){
+    alert(row + " "+ column + " "+ player);
+    blank[row][column]=player;
+    alert(blank);
+    winChecker();
+}
+function winChecker(){
+    alert("test win checker after a move");
+    for(var i=0; i<3;i++){
+        console.log(i);
+        if((blank[i][0] == blank[i][1])&&(blank[i][1]==blank[i][2])&&(blank[i][2]!=0)){
+            alert("all im saying is "+ blank[i][0]+ '='+blank[i][1]+ '='+blank[i][2]+"and they are not = 0");
+            alert("player: "+  blank[i][1] +"is the winner: "+ i);
+            return true
+        }
+        if((blank[0][i] == blank[1][i])&&(blank[1][i]==blank[2][i])&&(blank[i][2]!=0)){
+            alert("all im saying is "+ blank[i][0]+ '='+blank[i][1]+ '='+blank[i][2]+"and they are not = 0");
+            alert("player: "+  blank[i][1] +"is the winner: "+ i);
+            return true
+        }
+    }
+    return false
+}
 document.querySelector("#one").onclick = function(){
-    turnGiver();
+    
     if(turn){
     document.querySelector("#one").setAttribute("style", "background-color: green;")
     }
     else{
         document.querySelector("#one").setAttribute("style", "background-color: white;")
     }
+    marked(0,0,player);
+    turnGiver();
 }
 
 document.querySelector("#two").onclick = function(){
-    turnGiver();
+    
     if(turn){
     document.querySelector("#two").setAttribute("style", "background-color: green;")
     }
     else{
         document.querySelector("#two").setAttribute("style", "background-color: white;")
     }
+    marked(0,1,player);
+    turnGiver();
 }
 
 document.querySelector("#three").onclick = function(){
-    turnGiver();
+ 
     if(turn){
         document.querySelector("#three").setAttribute("style", "background-color: green;")
         }
         else{
             document.querySelector("#three").setAttribute("style", "background-color: white;")
         }
+        marked(0,2,player);
+        turnGiver();
 }
 
 document.querySelector("#four").onclick = function(){
-    turnGiver();
+    
     if(turn){
         document.querySelector("#four").setAttribute("style", "background-color: green;")
         }
         else{
             document.querySelector("#four").setAttribute("style", "background-color: white;")
         }
+        marked(1,0,player);
+        turnGiver();
 }
 
 document.querySelector("#five").onclick = function(){
-    turnGiver();
+
     if(turn){
         document.querySelector("#five").setAttribute("style", "background-color: green;")
         }
         else{
             document.querySelector("#five").setAttribute("style", "background-color: white;")
         }
+        marked(1,1,player);
+        turnGiver();
 }
 
 document.querySelector("#six").onclick = function(){
-    turnGiver();
+  
     if(turn){
         document.querySelector("#six").setAttribute("style", "background-color: green;")
         }
         else{
             document.querySelector("#six").setAttribute("style", "background-color: white;")
         }
+        marked(1,2,player);
+        turnGiver();
 }
 
 document.querySelector("#seven").onclick = function(){
-    turnGiver();
+ 
     if(turn){
         document.querySelector("#seven").setAttribute("style", "background-color: green;")
         }
         else{
             document.querySelector("#seven").setAttribute("style", "background-color: white;")
         }
+        marked(2,0,player);
+        turnGiver();
 }
 
 document.querySelector("#eight").onclick = function(){
-    turnGiver();
+
     if(turn){
         document.querySelector("#eight").setAttribute("style", "background-color: green;")
         }
         else{
             document.querySelector("#eight").setAttribute("style", "background-color: white;")
         }
+        marked(2,1,player);
+        turnGiver();
 }
 
 document.querySelector("#nine").onclick = function(){
-    turnGiver();
+
     if(turn){
         document.querySelector("#nine").setAttribute("style", "background-color: green;")
         }
         else{
             document.querySelector("#nine").setAttribute("style", "background-color: white;")
         }
+        marked(2,2,player);
+        turnGiver();
 }
 
 
